36945c7d99905460a6507cd16e0441202619cbde
YurkoLozitksy <32231740+YurkoLozitksy@users.noreply.github.com>
2018-02-24 20:47:40 -0500

    Lab 3

---
+++ Lab03-YurkoLozitksy/Lab03-YurkoLozitksy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab03_YurkoLozitksy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            int y = 15;
+            int z = 50;
+            int a = (x + y) * (z + 10);
+            Console.WriteLine("({0}+{1})*({2}+10) equals {3}", x, y, z, a);
+
+            Console.WriteLine("Hello\tWorld");
+            Console.WriteLine("Hello\nWorld");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello\\World");
+
+
+             
+        }
+    }
+}
