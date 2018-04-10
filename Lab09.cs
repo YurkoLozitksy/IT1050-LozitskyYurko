576c351426a20bfaff3e888c752481aa459ed4ba
YurkoLozitksy <32231740+YurkoLozitksy@users.noreply.github.com>
2018-04-09 21:46:40 -0400

    Lab09

---
+++ Lab09.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab09
+{
+    class MyMath
+    {
+        private double result;
+        private double operand1;
+        private double operand2;
+
+        public void Multiply(double operand1, double operand2)
+        {
+            result = operand1 * operand2;
+        }
+        public void Divide(double operand1, double operand2)
+        {
+            result = operand1 / operand2;
+        }
+        public void Add(double operand1, double operand2)
+        {
+            result = operand1 + operand2;
+        }
+        public void Subtract(double operand1, double operand2)
+        {
+            result = operand1 - operand2;
+        }
+        public double GetResult()
+        {
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+            math.Multiply(2.0, 3.0);
+            Console.WriteLine("The result of 2*3 is: {0}", math.GetResult());
+
+            math.Add(2.0, 3.0);
+            Console.WriteLine("The result of 2+3 is: {0}", math.GetResult());
+
+            math.Divide(2.0, 3.0);
+            Console.WriteLine("The result of 2/3 is: {0}", math.GetResult());
+
+            math.Subtract(2.0, 3.0);
+            Console.WriteLine("The result of 2-3 is: {0}", math.GetResult());
+
+        }
+    }
+
+}
